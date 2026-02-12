187c6d93188d72aeeb7cae1c2647878c7bdc96bd
arespawn <26845951+arespawn@users.noreply.github.com>
2026-02-12 17:31:10 +0200

    chore: bump version

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ if (!globalThis.crypto) {
 }
 
 (async () => {
-  const version = 'v2.1.6-alpha.5';
+  const version = 'v2.1.6-alpha.6';
   state.version = version;
   const streams = [
     { stream: pino.destination('logs.txt') },
